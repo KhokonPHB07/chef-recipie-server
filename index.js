024c0bbfd1db34436ab40c0db435d68341cdfae2
KhokonPHB07 <kshehala@gmail.com>
2023-05-23 08:07:08 +0600

    middlewear is added

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('chef is running');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`dragon is running on port : ${port}`)
+})
