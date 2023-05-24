0ae5431ce06cd67c26e5f3272ec83eb7601f9d78
KhokonPHB07 <kshehala@gmail.com>
2023-05-24 19:54:35 +0600

    recipie data adedd

--- index.js
+++ index.js
@@ -5,27 +5,33 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const chefBio = require('./Data/chef.json');
+const recipie = require('./Data/Recipie.json');
+
 app.get('/', (req, res) => {
     res.send('chef is running');
-})
+});
 
-const chefBio = require('./Data/chef.json');
-
-app.get('/chefBio',(req,res)=>{
+app.get('/chefBio', (req, res) => {
     res.send(chefBio);
+});
+
+app.get('/recipie',(req,res)=>{
+    res.send(recipie);
 })
 
+app.get('/chefBio/:id', async(req, res) => {
+    const id = req.params.id;
+    const chef = await chefBio.find((chef) => chef.id === id);
+    
 
-
-
-
-
-
-
-
-
-
+    if (!chef) {
+        res.status(404).send('Chef not found');
+    } else {
+        res.send(chef);
+    }
+});
 
 app.listen(port, () => {
-    console.log(`chef is running on port : ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
